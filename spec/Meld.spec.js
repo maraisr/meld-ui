1ee260627d6a7c7a158ace57692566e791292536
Marais Rossouw <marais@touchpointglobal.com>
2016-02-26 11:30:02 +1100

    Build scripts updated

--- spec/Meld.spec.js
+++ spec/Meld.spec.js
@@ -3,7 +3,12 @@ var Meld = Meld.Meld;
 describe('Testing the creation of a meld view', function () {
     it('Creates a new Meld instance', function () {
         var m = new Meld.Ui();
-       
+
         expect(m instanceof Meld.Ui).toBe(true);
     });
+
+	it('Creates a DOM node', function() {
+
+		expect(true).toBe(true);
+	});
 });
--- src/Meld.ts
+++ src/Meld.ts
@@ -3,7 +3,7 @@ import {Elements as e} from 'Elements';
 export module Meld {
     export class Ui {
         constructor(elm:HTMLElement) {
-            
+
         }
     }
 }
