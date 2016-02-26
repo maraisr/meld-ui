042b5d42594a6663491985e7dc0a26dfd8dab242
Marais Rossouw <me@maraisr.com>
2016-02-26 12:24:05 +1100

    More logic

--- src/Meld.ts
+++ src/Meld.ts
@@ -1,9 +1,21 @@
 import {Elements as e} from 'Elements';
 
 export module Meld {
-    export class Ui {
-        constructor(elm:HTMLElement) {
 
+	interface UiConfig {
+		selectTemplate:string;
+	}
+
+    export class Ui {
+		private config:UiConfig;
+		public elm:HTMLElement;
+
+        constructor(elm:HTMLElement, config:UiConfig) {
+			this.elm = elm;
+			this.config = config;
+
+			// TODO: This is only a test - remove later..
+			this.elm.innerHTML = config.selectTemplate;
         }
     }
 }
