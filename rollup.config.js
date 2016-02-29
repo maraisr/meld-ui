1a4a226042e0901043a48444059968c8ddb67fa4
Marais Rossouw <me@maraisr.com>
2016-03-01 08:36:29 +1100

    chore: added in sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,15 @@ import includePaths from 'rollup-plugin-includepaths';
 export default {
     entry: 'tmp/Meld.js',
     dest: 'dist/Meld.js',
-    plugins: [includePaths({
-        paths: ['tmp'],
-        extensions: ['.js']
-    }), babel()],
+    plugins: [
+		includePaths({
+			paths: ['tmp'],
+			extensions: ['.js']
+		}),
+		babel()
+	],
     format: 'umd',
     moduleName: 'Meld',
-    indent: false
+    indent: false,
+	sourceMap: 'inline'
 };
