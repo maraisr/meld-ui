8f40c42790e68cab9952e108ef02064a39cc1ba0
Marais Rossouw <me@maraisr.com>
2016-03-03 22:27:34 +1000

    0.0.13

--- dist/meld.min.js
+++ dist/meld.min.js
@@ -1,6 +1,6 @@
 /** meld-ui - The real world OIM library for JavaScript!
  *
- * @version v0.0.12
+ * @version v0.0.13
  * @author Marais Rossouw
  * @link https://github.com/maraisr/meld-ui
  * @license GPL-2.0
